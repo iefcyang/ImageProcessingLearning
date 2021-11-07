812bcbd23c5331bb355f03cb88a5978c2adfa6dc
Feng-Cheng Yang <iefcyang@ntu.edu.tw>
2021-11-07 22:53:25 +0800

    2021 11 07 NanKong

--- HWK03QT/main.cpp
+++ HWK03QT/main.cpp
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+    ColorImage* target;
+    ColorImage* real, * img;
+    real = HWK03QT::FFT(target, &img);
+
+
     QApplication a(argc, argv);
     //Second snd;
     HWK03QT w;
